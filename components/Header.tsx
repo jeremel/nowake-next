9c2b354a545d253b09615c0f8ad82cd556dd80e2
jeremel <jerlentz@gmail.com>
2023-01-20 17:19:56 -0500

    Fixed some spacing issues in the Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full h-full">
+    <header className="w-full md:h-screen h-full">
       <div className="h-full flex flex-wrap justify-between sm:gap-8 gap-0">
-        <div className="pl-6 md:max-w-[55%] w-auto place-self-center md:pt-0 pt-20 md:pb-0 pb-20">
+        <div className="md:pl-6 sm:px-8 px-6 md:max-w-[55%] w-auto place-self-center md:pt-0 pt-20 md:pb-0 pb-20">
           <p className="text-primary-navy font-serif font-light lg:text-5xl md:text-4xl text-3xl tracking-wide">
             <span className="text-primary-rust">No Wake</span> is a creative
             digital studio based out of southern New Jersey that builds websites
@@ -12,7 +12,7 @@ export default function Header() {
             experiences.
           </p>
         </div>
-        <div className="md:max-w-[40%] w-auto md:place-self-center place-self-end">
+        <div className="md:max-w-[40%] w-full md:place-self-center place-self-end">
           <Image
             src="/header-bg.jpg"
             alt="No Wake Studio logo"
